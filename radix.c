da8256cbd9373e52f25e6373cf55028034928139
mzzyk <zyk921109@126.com>
2014-12-10 17:12:50 +0800

    修改:         radix.c

--- radix.c
+++ radix.c
@@ -1,10 +1,13 @@
-//this ia an example of radix sort by pthread on linux
-// compile command :  g++ -o radix -pthread radix.c    # -pthread is imptant to program of pthread on linux
-// run command : ./radix 4 DATA1   #  4 is the number of pthread; DATA1 is a file of some integers to test
-#include<stdio.h>
-#include<stdlib.h>
-#include<pthread.h>
-#include<semaphore.h>
+/*程序功能描述如下：
+ * 利用MPI　对基数排序进行实现
+ *　编译命令：  mpicc -o radix radix.c
+ *　执行命令：  mpiexec.hydra -f /home/zyk/hostfile -n 4 ./radix DATA2
+ *	其中DATA2 为测试文件，里面有10000个int 型数据
+ *	４　为进程数目（或者是集群节点数目）；
+ * */
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define MAX 32
 #define MAX_SIZE 100000
@@ -19,55 +22,38 @@ int k;//the number of threads
 int max_num;
 int pow_10=1;
 
-pthread_mutex_t mutex;
-pthread_cond_t cond_var;
-pthread_t tid[MAX_SIZE];
-sem_t sem_i;
-sem_t sem_bucket;
-
 void get_max_num();
 void scan();
-//void get_sum(int pow_10);
-void *radix_sort(void *rank);
+void radix_sort(int rank);
 
-int main(int *argc,char *argv[]){
+int main(int argc,char *argv[]){
 	FILE *fp;
 	int i=0;
-	if((fp=fopen(argv[2],"r"))==NULL){
+	if((fp=fopen(argv[1],"r"))==NULL){
 		printf("can't open the file!");
-	exit(0);
+		exit(0);
 	}
 	while(fscanf(fp,"%d",&data[i])!=EOF)
-	i++;
+		i++;
 	n = i;
-	
-	k = (int)atoi(argv[1]);
-	
-	if((k <= 0)||(k >= MAX))
-	{
-		printf("invalid thread count -- defaulting to 4\n");
-		k = 4;
-	}
-	
+
 	get_max_num();
-	
-	sem_init(&sem_i,0,1);
-	sem_init(&sem_bucket,0,1);
 
-	for(i = 0; i < k; i++){    //create pthreads
-		sem_wait(&sem_i);
-		int tmp = i;
-		pthread_create(&tid[i],NULL,radix_sort,&tmp);
+	int rank;
+	MPI_Init(&argc, &argv);
+	// get the id of process
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	radix_sort(rank);
+
+	if(rank == 0){
+		printf("the smallest number is: %d\n",data[0]);
+		printf("the n/4 smallest number is: %d\n",data[n/4]);
+		printf("the n/2 smallest number is: %d\n",data[n/2]);
+		printf("the largest number is: %d\n",data[n - 1]);
+
 	}
 
-	for(i = 0; i < k; i++)   //destroy pthreads
-		pthread_join(tid[i],NULL);
-
-	printf("the smallest number is: %d\n",data[0]);
-	printf("the n/4 smallest number is: %d\n",data[n/4 - 1]);
-	printf("the n/2 smallest number is: %d\n",data[n/2 - 1]);
-	printf("the largest number is: %d\n",data[n - 1]);
-	
+	MPI_Finalize();
 	return 0;
 }
 
@@ -77,7 +63,7 @@ void get_max_num(){
 	max_num = data[0];
 	for(i = 0; i < n; i++)
 		if(data[i] > max_num)
-		max_num = data[i];
+			max_num = data[i];
 
 }
 
@@ -88,51 +74,61 @@ void scan(){
 	return;
 }
 
-void *radix_sort(void *rank){  //radix_sort
-	int my_rank = *((int *)rank);
-	sem_post(&sem_i);
-	
-	int i,j;
+void radix_sort(int rank)
+{  //radix_sort
+	int my_rank =rank;
+
+	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&pow_10, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(data, n, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(bucket, MAX_BUCKET, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Comm_size(MPI_COMM_WORLD,&k);
+
 	int my_first = my_rank * (n / k);
 	int my_last;
 	int size;
-	
+
 	if(my_rank == k-1)
 		my_last = n-1;
 	else
 		my_last = my_first + n/k -1;
 	size = my_last - my_first + 1;
+	int i;
+	//sleep(1);
+	while((max_num / pow_10) > 0)
+	{
 
-	while((max_num / pow_10) > 0){
-		for(i = my_first; i <= my_last; i++){
-		sem_wait(&sem_bucket);
-		bucket[(data[i] / pow_10) % MAX_BUCKET]++;   //get the sum 
-		sem_post(&sem_bucket);
+		for(i = my_first; i <= my_last; i++)
+		{
+			bucket[(data[i] / pow_10) % MAX_BUCKET]++;   //get the sum 
 		}
+		MPI_Barrier(MPI_COMM_WORLD);
+		int i;
 
-	pthread_mutex_lock(&mutex);
-	counter++;
-	if(counter == k){
-		counter = 0;
-		scan();
+		int rbucket[MAX_BUCKET];
+		for( i=0;i<MAX_BUCKET;i++)
+		{
+			MPI_Reduce(&bucket[i],&rbucket[i],1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+		}
+		if (rank == 0)
+		{
+			for(i=0;i<MAX_BUCKET;i++)
+			{
+				bucket[i]=rbucket[i];
+			}
+			scan();
 
-		for(i = (n - 1) ;i >= 0; i--) 
-			temp[--bucket[(data[i] / pow_10) % MAX_BUCKET]] = data[i];
+			for(i = (n - 1) ;i >= 0; i--) 
+				temp[--bucket[(data[i] / pow_10) % MAX_BUCKET]] = data[i];
 
-		for(i = 0; i < n; i++) //copy back
-			data[i] = temp[i];
-		
+			for(i = 0; i < n; i++) //copy back
+				data[i] = temp[i];
+		}
+		pow_10 *= 10;
 		for(i = 0; i < MAX_BUCKET; i++)
 			bucket[i] = 0; 
-		pow_10 *= 10;
 
-		pthread_cond_broadcast(&cond_var);
-	}
-	else{
-		while(pthread_cond_wait(&cond_var,&mutex) != 0);
-	}
-	pthread_mutex_unlock(&mutex);
-
+		MPI_Bcast(data, n, MPI_INT, 0, MPI_COMM_WORLD);
 	}
 
 }
